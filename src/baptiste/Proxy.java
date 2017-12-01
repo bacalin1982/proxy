0964ab3b9dced46180fe15b415c08f0a3939c75c
Frédéric Jamar <iSanzo@Mac-Pro-de-Frederic.local>
2017-12-01 21:04:45 +0100

    Adding configurable port feature by arg. Default port used is set to 8081.

--- src/baptiste/Proxy.java
+++ src/baptiste/Proxy.java
@@ -1,11 +1,19 @@
 package baptiste;
 
-
 public class Proxy {
 
     public static void main(String[] arg){
+        //Default port set to 8081
         int port = 8081;
-
+        //If a port was given by arg, replace default one.
+        if(arg.length == 1){
+            try{
+                port = Integer.parseInt(arg[0]);
+            }catch (NumberFormatException ex){
+                System.out.println("Wrong argument format. Default port 8081 will be used");
+            }
+        }
+        //Launching proxy cache server
         System.out.println("Proxy cache server on port["+port+"]");
         new Thread(new Server(port)).start();
     }
