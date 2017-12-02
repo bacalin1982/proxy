edfd0b3d7ecb6fa46842229dcf4a8dcfcf572cca
Frédéric Jamar <iSanzo@Mac-Pro-de-Frederic.local>
2017-12-02 22:45:40 +0100

    Implementing new class for constant messages

---
+++ src/baptiste/tools/Constants.java
@@ -0,0 +1,11 @@
+package baptiste.tools;
+
+public final class Constants {
+    /*
+    * Constant messages, if needed the char to replace by string is the % character.
+    * */
+    public static final String SERVER_START = "Starting server ...";
+    public static final String WRONG_ARG_FMT = "Wrong argument format. Default port 8081 will be used";
+    public static final String SERVER_START_PORT = "Proxy cache server on port[%]";
+    public static final String WRONG_ARG_NMB = "Proxy takes exactly 1 argument (% given). Use: Proxy <server_port>";
+}
